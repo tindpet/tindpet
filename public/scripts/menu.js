9d2efca4531c7eb1d63d77e6c6be59b316975841
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-02-26 17:29:40 +0100

    Submenu on avatar

---
+++ public/scripts/menu.js
@@ -0,0 +1,14 @@
+const avatar = document.querySelector(".avatar");
+const submenuBtn = document.querySelector(".submenu");
+
+let state = false;
+avatar.addEventListener("click", () => {
+
+  if (!state) {
+    submenuBtn.style.left = -100 + "px";
+    state = true;
+  } else {
+    submenuBtn.style.left = 70 + "px";
+    state = false;
+  }
+});
